f2328d39e857acb84ec79f63d746fecf986e78eb
pgreen <philip.green@q2ebanking.com>
2021-08-24 09:23:22 -0500

    removing inacurate tag

--- app/src/main/java/gishlabs/exandriapodcast/ExandriaPodcastsActivity.kt
+++ app/src/main/java/gishlabs/exandriapodcast/ExandriaPodcastsActivity.kt
@@ -6,8 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 
 class ExandriaPodcastsActivity : AppCompatActivity() {
 
-    private val TAG = "ListenNotes"
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
